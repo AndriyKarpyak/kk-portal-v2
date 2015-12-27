19a151d10b978fccbc2bce25032ea034733e6990
AndriyKarpyak <seraronax@gmail.com>
2015-12-27 12:34:00 +0200

    Added dummy data for chart
    Signed-off-by: AndriyKarpyak <seraronax@gmail.com>

--- src/com/kk/portal/server/ParkServerRestClient.java
+++ src/com/kk/portal/server/ParkServerRestClient.java
@@ -1,5 +1,6 @@
 package com.kk.portal.server;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.client.Client;
@@ -9,7 +10,10 @@ import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.kk.portal.shared.domain.comm.model.StationHighResDataResp;
+import com.kk.portal.shared.domain.comm.model.VariableHighResDataResp;
 import com.kk.portal.shared.domain.model.StationHighResData;
+import com.kk.portal.shared.domain.model.VariableHighResData;
 
 public class ParkServerRestClient {
 
@@ -19,22 +23,75 @@ public class ParkServerRestClient {
 
 	public List<StationHighResData> get() {
 
-		WebTarget target = client.target(getStartUrl);
-		Response response = target.request(MediaType.APPLICATION_JSON).get();
-		ParkResponseEntry<GetHighResStartResp> startResponse = response.readEntity(new GenericType<ParkResponseEntry<GetHighResStartResp>>() {});
-
-		target = client.target(getEndUrl + startResponse.Result.RequestGUID);
+		return mockResult();
 		
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		response = target.request(MediaType.APPLICATION_JSON).get();
-		ParkResponseList<StationHighResData> readEntity = response.readEntity(new GenericType<ParkResponseList<StationHighResData>>() {});
+//		WebTarget target = client.target(getStartUrl);
+//		Response response = target.request(MediaType.APPLICATION_JSON).get();
+//		ParkResponseEntry<GetHighResStartResp> startResponse = response.readEntity(new GenericType<ParkResponseEntry<GetHighResStartResp>>() {});
+//
+//		target = client.target(getEndUrl + startResponse.Result.RequestGUID);
+//		
+//		try {
+//			Thread.sleep(2000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//		response = target.request(MediaType.APPLICATION_JSON).get();
+//		ParkResponseList<StationHighResData> readEntity = response.readEntity(new GenericType<ParkResponseList<StationHighResData>>() {});
+//
+//		return readEntity.Result;
+	}
 
-		return readEntity.Result;
+	private List<StationHighResData> mockResult() {
+		List<StationHighResData> mockedResult = new ArrayList<StationHighResData>();
+		
+		StationHighResData mockedStationHighResData = new StationHighResDataResp();
+		
+		mockedStationHighResData.setStationGUID("67dd1cd1-d551-4087-9ac1-fb74a4277d97");
+		mockedStationHighResData.setStationName("Mocked Station");
+		
+		List<VariableHighResData> mockedVariableHighResDataList = new ArrayList<VariableHighResData>();
+		
+		VariableHighResData mockedVariableHighResData = new VariableHighResDataResp();
+		
+		mockedVariableHighResData.setMappingGUID("1c855de9-75d7-425c-a8ef-3974385a2358");
+		mockedVariableHighResData.setMappingName("Mocked wind speed");
+		mockedVariableHighResData.setUnits("m/s");
+		mockedVariableHighResData.setTotalValues(10);
+		mockedVariableHighResData.setTimeStamps(createMockedTimeStamps());
+		mockedVariableHighResData.setValues(createMockedValues());
+		
+		mockedVariableHighResDataList.add(mockedVariableHighResData);
+		
+		mockedStationHighResData.setDataElementLogs(mockedVariableHighResDataList);
+		
+		mockedResult.add(mockedStationHighResData);
+		return mockedResult;
+	}
+
+	private List<Double> createMockedValues() {
+		List<Double> res = new ArrayList<Double>();
+		res.add(0.);
+		res.add(0.5);
+		res.add(1.);
+		res.add(1.5);
+		res.add(2.);
+		res.add(2.5);
+		res.add(3.);
+		return res;
+	}
+
+	private List<String> createMockedTimeStamps() {
+		List<String> res = new ArrayList<String>();
+		res.add("2015-10-20 10:00:00");
+		res.add("2015-10-20 10:00:10");
+		res.add("2015-10-20 10:00:20");
+		res.add("2015-10-20 10:00:30");
+		res.add("2015-10-20 10:00:40");
+		res.add("2015-10-20 10:00:50");
+		res.add("2015-10-20 10:00:60");
+		return res;
 	}
 }
